d057813a6517d9bfa2a5b7516a29a1c9ebc97206
Artur Rocha <arthurrocha_@hotmail.com>
2018-07-10 09:26:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from bs4 import BeautifulSoup
 import urllib.request, requests
 import telegram
 
-# TOKEN E.g. 675987757:BAzVnw-c5D2PeWtnTtxS-li90l3IJBV-Nbc
-bot = telegram.Bot(token='YOUR TOKEN')
-
-#<td class="left"><b class="tablesaw-cell-label">Date</b>12&nbsp;February 2018</td>
+# Bot token example = '675987757:BAzVnw-c5D2PeWtnTtxS-li90l3IJBV-Nbc'
+bot_token = 'your bot token'
+# Telegram chat id of targets to receive messages
+telegram_chat_id = ['xxxxxxxxx', 'xxxxxxxxx']
 
 class AppURLopener(urllib.request.FancyURLopener):
     version = 'Mozilla/5.0'
 
+
+bot = telegram.Bot(token=bot_token)
 url = "https://dbei.gov.ie/en/What-We-Do/Workplace-and-Skills/Employment-Permits/Current-Application-Processing-Dates"
 
 opener = AppURLopener()
@@ -21,7 +23,6 @@ result_search = str(soup.find("table", {"id": "table-196"}))
 result_search = result_search.split("td")
 result_search = result_search[-2].replace('class="left">', '').replace('</', '')
 
-# telegram id of targets to receive
-telegram_chat_id = ['xxxxxxxxx', 'xxxxxxxxx']
+
 for user in telegram_chat_id:
 	bot.send_message(chat_id=user, text="DBEI processing = {}".format(result_search))
